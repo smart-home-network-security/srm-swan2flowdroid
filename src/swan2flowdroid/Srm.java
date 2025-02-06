57bf47c8ab6c4915a978327789f6ffbf0ff885f8
François De Keersmaeker <francois.dekeersmaeker@uclouvain.be>
2025-02-06 12:29:40 +0100

    Terminology: SRM type -> SRM class

--- src/swan2flowdroid/Srm.java
+++ src/swan2flowdroid/Srm.java
@@ -11,19 +11,19 @@ import com.fasterxml.jackson.annotation.JsonProperty;
  */
 class Srm {
     
-    // SRM types for SWAN
-    protected static final String TYPE_SWAN_SOURCE = "source";
-    protected static final String TYPE_SWAN_SINK = "sink";
-    // SRM types for FlowDroid
-    protected static final String TYPE_FLOWDROID_SOURCE = "_SOURCE_";
-    protected static final String TYPE_FLOWDROID_SINK = "_SINK_";
-    protected static final String TYPE_FLOWDROID_BOTH = "_BOTH_";
+    // SRM classes for SWAN
+    protected static final String CLASS_SWAN_SOURCE = "source";
+    protected static final String CLASS_SWAN_SINK = "sink";
+    // SRM classes for FlowDroid
+    protected static final String CLASS_FLOWDROID_SOURCE = "_SOURCE_";
+    protected static final String CLASS_FLOWDROID_SINK = "_SINK_";
+    protected static final String CLASS_FLOWDROID_BOTH = "_BOTH_";
 
     // Relevant SWAN JSON fields
     @JsonProperty("name")
     protected String   name;
     @JsonProperty("srm")
-    protected String[] types;
+    protected String[] classes;
     @JsonProperty("parameters")
     protected String[] parameters;
     @JsonProperty("return")
@@ -36,11 +36,11 @@ class Srm {
      * @return True if this SRM is a source, false otherwise.
      */
     protected boolean isSource() {
-        if (this.types == null) {
+        if (this.classes == null) {
             return false;
         }
 
-        return Arrays.asList(this.types).contains(Srm.TYPE_SWAN_SOURCE);
+        return Arrays.asList(this.classes).contains(Srm.CLASS_SWAN_SOURCE);
     }
 
     /**
@@ -49,11 +49,11 @@ class Srm {
      * @return True if this SRM is a sink, false otherwise.
      */
     protected boolean isSink() {
-        if (this.types == null) {
+        if (this.classes == null) {
             return false;
         }
         
-        return Arrays.asList(this.types).contains(Srm.TYPE_SWAN_SINK);
+        return Arrays.asList(this.classes).contains(Srm.CLASS_SWAN_SINK);
     }
 
     /**
@@ -65,7 +65,7 @@ class Srm {
     public String toString() {
         return String.format("%s %s %s %s",
             this.name,
-            Arrays.toString(this.types),
+            Arrays.toString(this.classes),
             Arrays.toString(this.parameters),
             this.returnType
         );
@@ -105,7 +105,7 @@ class Srm {
             }
         }
 
-        // Return type
+        // Return class
         String returnTypeLocal = this.returnType == null ? "void" : this.returnType;
         strFlowdroid = String.format("<%s: %s %s(", packageName, returnTypeLocal, methodName);
 
@@ -118,15 +118,15 @@ class Srm {
             strFlowdroid += param;
         }
 
-        // SRM type
-        List typesList = Arrays.asList(this.types);
-        if (typesList.contains(Srm.TYPE_SWAN_SOURCE) &&
-            typesList.contains(Srm.TYPE_SWAN_SINK)) {
-            strFlowdroid += String.format(")> -> %s", Srm.TYPE_FLOWDROID_BOTH);
-        } else if (typesList.contains(Srm.TYPE_SWAN_SOURCE)) {
-            strFlowdroid += String.format(")> -> %s", Srm.TYPE_FLOWDROID_SOURCE);
-        } else if (typesList.contains(Srm.TYPE_SWAN_SINK)) {
-            strFlowdroid += String.format(")> -> %s", Srm.TYPE_FLOWDROID_SINK);
+        // SRM class
+        List classesList = Arrays.asList(this.classes);
+        if (classesList.contains(Srm.CLASS_SWAN_SOURCE) &&
+            classesList.contains(Srm.CLASS_SWAN_SINK)) {
+            strFlowdroid += String.format(")> -> %s", Srm.CLASS_FLOWDROID_BOTH);
+        } else if (classesList.contains(Srm.CLASS_SWAN_SOURCE)) {
+            strFlowdroid += String.format(")> -> %s", Srm.CLASS_FLOWDROID_SOURCE);
+        } else if (classesList.contains(Srm.CLASS_SWAN_SINK)) {
+            strFlowdroid += String.format(")> -> %s", Srm.CLASS_FLOWDROID_SINK);
         }
 
         return strFlowdroid;
